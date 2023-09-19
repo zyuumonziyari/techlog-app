48ce2de930b82a15a3999d9ebfbbf508b452e3fa
s-shiho <sou0921@fuji.waseda.jp>
2023-09-19 17:25:52 +0900

    Rspecの初期設定を完了

--- Gemfile
+++ Gemfile
@@ -51,6 +51,10 @@ gem 'bootsnap', require: false
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[mri mingw x64_mingw]
+  gem 'rspec-rails' # 追加
+  gem 'factory_bot_rails' # 追加
+
+
 end
 
 group :development do
@@ -60,6 +64,8 @@ group :development do
   gem 'rubocop-rails', require: false # 追加
   gem 'rubocop-rspec' # 追加
   gem 'web-console'
+  gem 'spring-commands-rspec' # 追記
+
 
   # Add speed badges [https://github.com/MiniProfiler/rack-mini-profiler]
   # gem "rack-mini-profiler"
--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,16 @@ module TechlogApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.generators do |g| # ここから追記
+      g.assets false          # CSS, JavaScriptファイルを自動生成しない
+      g.helper     false      # helperファイルを自動生成しない
+      g.test_framework :rspec, # ここから5行を追記
+        fixtures: false, # テストDBにレコードを作るfixtureの作成をスキップ(FactoryBotを使用するため)
+        view_specs: false, # ビューファイル用のスペックを作成しない
+        helper_specs: false, # ヘルパーファイル用のスペックを作成しない
+        routing_specs: false # routes.rb用のスペックファイル作成しない
+    end  # ここまで追記
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -31,7 +41,6 @@ module TechlogApp
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    # Don't generate system test files.
-    config.generators.system_tests = nil
+    config.generators.system_tests = 'test_framework: :rspec'
   end
 end
