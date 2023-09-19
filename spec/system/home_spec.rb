337170ab4ba118890472921a9280978d80fc303c
s-shiho <sou0921@fuji.waseda.jp>
2023-09-19 19:07:59 +0900

    Capybaraのインストール

--- spec/system/home_spec.rb
+++ spec/system/home_spec.rb
@@ -2,8 +2,12 @@ require 'rails_helper'
 
 RSpec.describe 'Home', type: :system do
   before do
-    driven_by(:rack_test)
+    driven_by :selenium_chrome_headless
   end
+  
+  before do # ここから追記
+    driven_by :selenium_chrome_headless
+  end # ここまで追記
 
   describe 'トップページの検証' do
     it 'Home#top という文字列が表示される' do
